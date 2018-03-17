465616448103e19325d2a8afe037c535146460fc
Damian Drożdż <damiandrozdz@interia.pl>
2018-03-17 16:13:15 +0100

    + Ads

--- app/src/main/java/pl/derankingteam/aplikacja/MainActivity.java
+++ app/src/main/java/pl/derankingteam/aplikacja/MainActivity.java
@@ -1,6 +1,11 @@
 package pl.derankingteam.aplikacja;
 
 import android.graphics.Color;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,15 +18,21 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.MobileAds;
+
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
     private DrawerLayout mDrawerLayout;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -61,7 +72,41 @@ public class MainActivity extends AppCompatActivity
 
 
         );
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
